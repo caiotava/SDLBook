9681c91edca0817066ada3af988a3b35ebf23db4
Caio César Tavares <caiotava@gmail.com>
2015-06-04 16:47:38 -0300

    Include hello world from chapter 1 and Makefile.

---
+++ chapter1/hello-world-1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<SDL2/SDL.h>
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+
+int main(int argc, char **args)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    const char windowTitle[] = "Chapter 1: Setting up SDL";
+    window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_Quit();
+
+    return 0;
+}
