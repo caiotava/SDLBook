85200eb93040e5c59e300aa0fc994f5f9bb98be6
Caio César Tavares <caiotava@gmail.com>
2015-06-14 20:21:41 -0300

    Include TextureManager class.

---
+++ chapter2/TextureManager.h
@@ -0,0 +1,20 @@
+#ifndef __TEXTURE_MANAGER__
+#define __TEXTURE_MANAGER__
+
+#include<iostream>
+#include<map>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+
+class TextureManager
+{
+    public:
+        bool load(std::string fileName, std::string id, SDL_Renderer *renderer);
+        void draw(std::string id, int x, int y, int width, int height, SDL_Renderer *renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+        void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+    private:
+        std::map<std::string, SDL_Texture*> textureMap;
+};
+
+#endif
