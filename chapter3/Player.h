9fd15678a5f149c5ddc5793541c4f3262802e2dd
Caio César Tavares <caiotava@gmail.com>
2015-06-21 00:40:12 -0300

    Fix class name and remove GameObject::clean call.

--- chapter3/Player.h
+++ chapter3/Player.h
@@ -4,13 +4,12 @@
 #include<iostream>
 #include"GameObject.h"
 
-class PLayer: public GameObject
+class Player: public GameObject
 {
     public:
         void update();
         void clean()
         {
-            GameObject::clean();
             std::cout << "clean PLayer";
         }
 };
